f6029a323ab37289b26d875e2b25d695b38a1cc7
gapoorva <gapoorva@umich.edu>
2016-05-01 00:34:14 -0400

    adding a bunch of js/css libraries

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
-<p> It works! </p>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="author" content="Apoorva Gupta">
+		<meta name="description" content="The personal website of Apoorva Gupta, Software Developer.">
+		<meta keywords="apoorva, gupta, software, developer, michigan, computer, science, programer, projects, project">
+		<meta http-equiv="content-type" content="text/html;charset=UTF-8">
+		<meta property="og:url" content="http://www.apoorvagupta.com">
+		<meta property="og:title" content="Apoorva Gupta, Software Developer">
+		<meta property="og:description" content="The personal website of Apoorva Gupta, Software Developer.">
+		<meta property="og:site_name" content="Apoorva Gupta">
+		<!-- TODO:
+		<meta property="og:image" content="???">
+		 -->
+
+		<title>Apoorva Gupta</title>
+
+		<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon"/>
+
+		<link rel="stylesheet" type="text/css" href="css/jquery-ui.css"/>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css"/>
+
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/jquery-ui.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+		
+	</head>
+	<body>
+		<p> still working! </p>
+	</body>
+</html>
+
