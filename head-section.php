1de634f174e0fe0b06a2c65e6a216fa2f2842ce4
gapoorva <gapoorva@umich.edu>
2016-05-01 01:39:02 -0400

    Added head section template and started home page

---
+++ head-section.php
@@ -0,0 +1,63 @@
+<?php 
+	echo '<head>
+		<meta charset="utf-8">
+		<meta name="author" content="Apoorva Gupta">
+		<meta name="description" content="The personal website of Apoorva Gupta, Software Developer.">
+		<meta keywords="apoorva, gupta, software, developer, michigan, computer, science, programer, projects, project">
+		<meta http-equiv="content-type" content="text/html;charset=UTF-8">
+		<meta property="og:url" content="http://www.apoorvagupta.com">
+		<meta property="og:title" content="Apoorva Gupta, Software Developer">
+		<meta property="og:description" content="The personal website of Apoorva Gupta, Software Developer.">
+		<meta property="og:site_name" content="Apoorva Gupta">
+		<!-- TODO:
+		<meta property="og:image" content="???">
+		 -->
+
+		<title>Apoorva Gupta</title>
+
+		<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon"/>
+
+		<link rel="stylesheet" type="text/css" href="css/jquery-ui.css"/>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css"/>
+
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/jquery-ui.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<style> 
+			body {
+				font-family: Calibri, Candara, Segoe, \'Segoe UI\', Optima, Arial, sans-serif;
+			}
+
+			div.title {
+				border-bottom: 2px solid #4CAEE3;
+				padding: 3% 7%;
+			}
+
+			h1.huge {
+				font-size: 400%;
+			}
+
+			/* Small devices (tablets, 768px and up) */
+			@media (min-width: @screen-sm-min) { 
+				div.title {
+					padding: 2% 7%;
+				}
+			}
+
+			/* Medium devices (desktops, 992px and up) */
+			@media (min-width: @screen-md-min) {
+				div.title {
+					padding: 1% 5%;
+				}
+			}
+
+			/* Large devices (large desktops, 1200px and up) */
+			@media (min-width: @screen-lg-min) { 
+				div.title {
+					padding-left: 3%;
+				}
+			}
+		</style>
+	</head>';
+?>
--- index.php
+++ index.php
@@ -1,35 +1,16 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<meta name="author" content="Apoorva Gupta">
-		<meta name="description" content="The personal website of Apoorva Gupta, Software Developer.">
-		<meta keywords="apoorva, gupta, software, developer, michigan, computer, science, programer, projects, project">
-		<meta http-equiv="content-type" content="text/html;charset=UTF-8">
-		<meta property="og:url" content="http://www.apoorvagupta.com">
-		<meta property="og:title" content="Apoorva Gupta, Software Developer">
-		<meta property="og:description" content="The personal website of Apoorva Gupta, Software Developer.">
-		<meta property="og:site_name" content="Apoorva Gupta">
-		<!-- TODO:
-		<meta property="og:image" content="???">
-		 -->
+	<?php include 'head-section.php';?>
 
-		<title>Apoorva Gupta</title>
-
-		<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon"/>
-
-		<link rel="stylesheet" type="text/css" href="css/jquery-ui.css"/>
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
-		<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css"/>
-
-		<script type="text/javascript" src="js/jquery.min.js"></script>
-		<script type="text/javascript" src="js/jquery-ui.min.js"></script>
-		<script type="text/javascript" src="js/bootstrap.min.js"></script>
-
-		
-	</head>
 	<body>
-		<p> still working! </p>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xs-offset-1 col-xs-10 title">
+					<h1 class="huge">Apoorva Gupta</h1>
+				</div>
+			</div>	
+		</div>
+		
 	</body>
 </html>
 
