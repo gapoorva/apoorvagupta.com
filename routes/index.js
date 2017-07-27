ec84ca56d2d632a1cda7bca7770a088ff9b405b3
gapoorva <gapoorva@umich.edu>
2017-07-26 20:34:47 -0700

    Added an undercontruction view and labeled index as underconstruction
    This should suffice as the website is currently down until a new landing page is developed. But lol that won't happen for a while

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var context = {
+    title: 'Express'
+  }
+  res.render('underconstruction', context);
 });
 
 module.exports = router;
